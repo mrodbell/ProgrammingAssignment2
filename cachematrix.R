987286fe7d7e6721fcb858d5bdce5fa2db155b60
Mike Rodbell <mikerodbell@gmail.com>
2014-06-09 14:46:28 -0400

    Put in pseudo code and comments
    Copied info from examples as a starter. Need to modify so that it
    manipulates matrix and does the inversion rather than calculating the
    mean of an array (as per the original example).

--- cachematrix.R
+++ cachematrix.R
@@ -6,14 +6,35 @@
 ## Author: Mike Rodbell
 ## 
 
-## Write a short comment describing this function
+## 
+##  Function: makeCacheMatrix
+##
+##  Purpose:  makeCacheMatrix contains a set of functions to create
+##            and manipulate 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## stuff from makeVector
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+##
+##  Function: cacheSolve
+##
+##  Purpose:  returns the inverted case of the input matrix. To help 
+##            alleviate processing time, it will cache the results to 
+##            simply return the last operation
+##
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
