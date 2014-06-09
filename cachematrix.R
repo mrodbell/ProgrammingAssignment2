ec9624cb5efb5d0a327fc27684676c38650a1c7a
Mike Rodbell <mikerodbell@gmail.com>
2014-06-08 20:01:48 -0400

    First edits
    Simply some comments to see how this works with the new tools

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## File: cacheMatrix
+##
+## Author: Mike Rodbell
+## 
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
